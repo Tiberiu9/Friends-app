8a5d0f57fa4946f81543c92185b209557b8dc45e
Tibi <tyby_tiberiu@yahoo.com>
2023-05-18 17:37:14 +0300

    Modified visuals

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 200 }, format: { with: URI::MailTo::EMAIL_REGEXP }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
          has_many :friends    
-         
+  
 end
